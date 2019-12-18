7ec0a7f13f7291493114616be5399dc173ee52de
晨阳 <892474885@qq.com>
2019-12-18 19:36:05 +0800

    likejs init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./assets/img/logo512.png";
+import './assets/css/index.css';
+
+ReactDOM.render(
+  <>
+    <h1>123</h1>
+    <img src={logo} alt="logo" />
+  </>,
+  document.getElementById("root")
+);
